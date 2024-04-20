c38a4d43e306e0418f7cbaccdb3be0cd6b57e242
Henrique RM88273 <101111279+Mechamorick@users.noreply.github.com>
2024-04-19 22:30:11 -0300

    Add files via upload

---
+++ fiap-checkpoint2-main/src/main/java/br/com/fiap/checkpoint2/Checkpoint2.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class checkpoint2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(checkpoint2.class, args);
+	}
+
+}
